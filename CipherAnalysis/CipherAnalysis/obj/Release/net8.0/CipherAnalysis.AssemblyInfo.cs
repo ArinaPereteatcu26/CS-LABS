fd1898ca3743771b494cc5a57201c3ce1ddd2328
Arina <arinapereteatcu0@gmail.com>
2024-10-21 00:48:24 +0300

    Add report

--- CipherAnalysis/CipherAnalysis/obj/Release/net8.0/CipherAnalysis.AssemblyInfo.cs
+++ CipherAnalysis/CipherAnalysis/obj/Release/net8.0/CipherAnalysis.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CipherAnalysis")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a80fcdf01dba47ba4f615f4aacb2335d53614b7e")]
 [assembly: System.Reflection.AssemblyProductAttribute("CipherAnalysis")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CipherAnalysis")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
